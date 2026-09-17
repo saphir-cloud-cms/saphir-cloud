810bd694dcf52fc01fa09675a266ca8889fda40f
saphir-cloud-cms <flowersnjelly@gmail.com>
2026-09-17 20:21:40 +0700

    Delete my-saphir-cloud-web directory

--- my-saphir-cloud-web/js/main.js
+++
@@ -1,9 +0,0 @@
-// Hàm hỗ trợ cuộn mượt đến các section khi click thẻ card
-function scrollToSection(sectionId) {
-    const element = document.getElementById(sectionId);
-    if (element) {
-        element.scrollIntoView({
-            behavior: 'smooth'
-        });
-    }
-}
